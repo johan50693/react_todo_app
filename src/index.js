350b05d1ad044a606ec865a2f802b292ae542e0c
Albert Urbina <aurbina@thefactoryhka.com>
2024-09-06 18:27:09 -0400

    High Order Components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/index.js';
+// import App from './App/index.js';
 import './index.css';
 
+
+const App = ({nombre, saludo}) => {
+
+  return (
+    <p>!{saludo}!, {nombre}!!!</p>
+  )
+}
+
+function withWhatever (WrappedComponent) {
+  return function ComponenteDeVerdad (props) {
+    return (
+      <>
+        <WrappedComponent {...props}/>
+        <h1>Estamos acompañando al wrappedcomponent</h1>
+      </>
+    )
+  
+  }
+}
+
+const AppWithWatever = withWhatever(App);
+
 ReactDOM.render(
-  <App />,
+  // <App saludo="Buenas" nombre="Pepe" />,
+  <AppWithWatever saludo="Epaleee" nombre="Pedrooo" />,
   document.getElementById('root')
 );
